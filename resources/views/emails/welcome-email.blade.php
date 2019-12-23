e810e07e0c2cb56e639802bb5e1c13e242df1d41
Laurene <bidaux.laurene@gmail.com>
2019-12-23 14:59:50 +0100

    tuto insta

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to FreeCodeGram
+
+This is a community of fellow developers and we love that you have joined us.
+
+All the best !
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
